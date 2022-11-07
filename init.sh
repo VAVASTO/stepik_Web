1f66020fd8738b7aba8b1b8894d486562aead61e
VAVASTO <vladimir.polotsk@gmail.com>
2022-11-07 10:58:34 +0300

    updated init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 sudo ln -s /home/box/web/nginx.conf /etc/nginx/sites-enable/nginx.conf
 sudo ln -s /home/box/web/nginx.conf /etc/nginx/nginx.conf
-sudo systemctl restart nginx
+sudo rm -rf /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
 gunicorn --bind 0.0.0.0:8080 hello:app
